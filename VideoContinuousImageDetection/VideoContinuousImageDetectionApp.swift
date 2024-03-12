318896b06a7a40cfec4d80952ef64d9d60388206
Danika Gupta <danika@gprof.com>
2024-03-12 15:30:14 -0700

    Initial Commit

---
+++ VideoContinuousImageDetection/VideoContinuousImageDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoContinuousImageDetectionApp.swift
+//  VideoContinuousImageDetection
+//
+//  Created by Amit Gupta on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoContinuousImageDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
